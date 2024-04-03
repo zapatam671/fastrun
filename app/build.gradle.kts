cadf1920007a0368a4bfb7bf5bc3c555833741f3
zapatam671 <mattzapata671@gmail.com>
2024-04-03 17:37:11 +1000

    add some implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
 }
