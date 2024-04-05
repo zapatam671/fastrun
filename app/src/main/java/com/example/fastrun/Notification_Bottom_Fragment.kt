38e68f29cd8df5e584e9dbf3180e90bd5d5ca980
zapatam671 <mattzapata671@gmail.com>
2024-04-06 03:06:28 +1000

    create notifications fragment, need to add button funciton

--- app/src/main/java/com/example/fastrun/Notification_Bottom_Fragment.kt
+++ app/src/main/java/com/example/fastrun/Notification_Bottom_Fragment.kt
@@ -5,28 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fastrun.adaptar.NotificationAdapter
+import com.example.fastrun.databinding.FragmentNotificationBottomBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Notification_Bottom_Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Notification_Bottom_Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class Notification_Bottom_Fragment : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentNotificationBottomBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,26 +23,26 @@ class Notification_Bottom_Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification__bottom_, container, false)
+        binding = FragmentNotificationBottomBinding.inflate(layoutInflater,container,false)
+        val notifications = listOf(
+            "Your order has been Canceled Successfully",
+            "Order has been taken by the barista",
+            "Congrats Your Order Placed"
+        )
+        val notificationImage = listOf(
+            R.drawable.sademoji,
+            R.drawable.truck,
+            R.drawable.confirm
+        )
+        val adapter = NotificationAdapter(
+            ArrayList(notifications),
+            ArrayList(notificationImage)
+        )
+
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Notification_Bottom_Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Notification_Bottom_Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
