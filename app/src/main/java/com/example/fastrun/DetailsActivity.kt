b4dd48693bd2fe24ba79e53091a26cf5ef070556
zapatam671 <mattzapata671@gmail.com>
2024-04-08 21:49:02 +1000

    save current progress on food details

--- app/src/main/java/com/example/fastrun/DetailsActivity.kt
+++ app/src/main/java/com/example/fastrun/DetailsActivity.kt
@@ -5,16 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fastrun.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+        val foodName = intent.getStringExtra("MenuItemName")
+        val foodImage = intent.getIntExtra("MenuItemImage", 0)
+        binding.detailFoodName.text = foodName
+        binding.DetailFoodImage.setImageResource(foodImage)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
