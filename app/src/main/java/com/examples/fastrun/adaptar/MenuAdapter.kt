1a6e97832378232d34b976be03140c7dbb9ae2fa
unknown <mvop19@gmail.com>
2024-04-19 03:49:06 +1000

    started on menuAdapter.kt changes

--- app/src/main/java/com/examples/fastrun/adaptar/MenuAdapter.kt
+++ app/src/main/java/com/examples/fastrun/adaptar/MenuAdapter.kt
@@ -2,15 +2,19 @@ package com.examples.fastrun.adaptar
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fastrun.databinding.MenuItemBinding
 import com.examples.fastrun.DetailsActivity
+import com.examples.fastrun.model.MenuItem
 
-class MenuAdapter(private val menuItemsName: List<String>, private val menuItemPrice: List<String>, private val MenuImage: List<Int>, private val requireContext: Context) :RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
+class MenuAdapter(
+    private val menuItems:List<MenuItem>,
+    private val requireContext: Context
+) :RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
 
-private val itemClickListener: OnClickListener ?= null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
         val binding = MenuItemBinding.inflate(LayoutInflater.from(parent.context),parent, false)
         return MenuViewHolder(binding)
@@ -20,32 +24,44 @@ private val itemClickListener: OnClickListener ?= null
     override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
         holder.bind(position)
     }
-    override fun getItemCount(): Int = menuItemsName.size
+    override fun getItemCount(): Int = menuItems.size
     inner class MenuViewHolder(private val binding: MenuItemBinding) :RecyclerView.ViewHolder(binding.root) {
         init {
             binding.root.setOnClickListener {
                 val position = adapterPosition
                 if (position != RecyclerView.NO_POSITION){
-                    itemClickListener?.onItemClick(position)
+                    openDetailActivity(position)
                 }
-                // setonclick listener to open details
-
-                val intent = Intent(requireContext, DetailsActivity::class.java)
-                intent.putExtra("MenuItemName", menuItemsName.get(position))
-                intent.putExtra("MenuItemImage", MenuImage.get(position))
-                requireContext.startActivity(intent)
             }
         }
-        fun bind(position: Int) {
-            binding.apply {
-                menuFoodName.text=menuItemsName[position]
-                menuPrice.text = menuItemPrice[position]
-                menuImage.setImageResource(MenuImage[position])
 
+        private fun openDetailActivity(position: Int) {
+val menuItem :MenuItem = menuItems[position]
+
+            // a intent to open details activity and pass data
+            val intent = Intent(requireContext ,DetailsActivity::class.java).apply {
+                putExtra("MenuItemName",menuItem.foodName)
+                putExtra("MenuItemImage",menuItem.foodImageUrl)
+                putExtra("MenuItemDescription",menuItem.foodDescription)
+                putExtra("MenuItemIngredients",menuItem.foodIngredients)
+                putExtra("MenuItemPrice",menuItem.foodPrice)
+
+            }
+            // start the detail activity
+            requireContext.startActivity(intent)
+        }
+
+        fun bind(position: Int) {
+            val menuItem = menuItems[position]
+            binding.apply {
+                menuFoodName.text=menuItem.menuFoodName
+                menuPrice.text = menuItem.foodPrice
+                val Uri = Uri.parse(menuItem.foodImageUrl)
             }
         }
 
     }
+
     interface OnClickListener{
         fun onItemClick(position: Int)
 
---
+++ app/src/main/java/com/examples/fastrun/model/MenuItem.kt
@@ -0,0 +1,10 @@
+package com.examples.fastrun.model
+
+data class MenuItem(
+    val foodName :String?=null,
+    val foodPrice :String?=null,
+    val foodDescription :String?=null,
+    val foodImageUrl :String?=null,
+    val foodIngredients :String?=null,
+
+    )
