c42e74d25bf745589e1811b70c623e2ce3e104e8
unknown <mvop19@gmail.com>
2024-04-22 23:39:20 +1000

    connected cart to firebase

--- app/src/main/java/com/examples/fastrun/DetailsActivity.kt
+++ app/src/main/java/com/examples/fastrun/DetailsActivity.kt
@@ -2,10 +2,14 @@ package com.examples.fastrun
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.fastrun.databinding.ActivityDetailsBinding
+import com.examples.fastrun.model.CartItems
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 
 class DetailsActivity : AppCompatActivity() {
@@ -15,13 +19,15 @@ class DetailsActivity : AppCompatActivity() {
     private var foodDescription: String? = null
     private var foodIngredient: String? = null
     private var foodPrice: String? = null
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-
+        // initialize FirebaseAuth
+        auth = FirebaseAuth.getInstance()
         foodName = intent.getStringExtra("MenuItemName")
         foodDescription = intent.getStringExtra("MenuItemDescription")
         foodIngredient = intent.getStringExtra("MenuItemIngredients")
@@ -43,7 +49,24 @@ class DetailsActivity : AppCompatActivity() {
         binding.imageButton.setOnClickListener {
             finish()
         }
+        binding.addItemButton.setOnClickListener {
+            addItemToCart()
+        }
 
+    }
 
+    private fun addItemToCart() {
+        val database = FirebaseDatabase.getInstance().reference
+        val userId = auth.currentUser?.uid?:""
+
+//Create a cartItems object
+val cartItem =CartItems(foodName.toString(),foodPrice.toString(),foodDescription.toString(),foodImage.toString(),1)
+
+        // save data to cart item to firebase database
+        database.child("user").child(userId).child("CartItems").push().setValue(cartItem).addOnSuccessListener {
+            Toast.makeText(this, "Items added into cart successfully", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this, "Item Not added", Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/examples/fastrun/model/CartItems.kt
@@ -0,0 +1,13 @@
+package com.examples.fastrun.model
+
+data class CartItems(
+    var foodName: String ?=null,
+    var foodPrice: String ?=null,
+    var foodDescription: String ?=null,
+    var foodImage: String ?=null,
+    var foodQuantity: Int ?=null
+)
+
+
+
+
